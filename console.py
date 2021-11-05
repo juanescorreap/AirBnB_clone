935a12aae30859bf09c904a958b2b71d8bcf2171
John Doe <juanestebancope@outlook.com>
2021-11-04 21:13:21 -0500

    task 7

--- console.py
+++ console.py
@@ -3,7 +3,9 @@
 Class that defines HBNBCommand
 """
 import cmd
-
+from models import storage
+from models.base_model import BaseModel
+from models.engine import file_storage
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -29,9 +31,61 @@ class HBNBCommand(cmd.Cmd):
         """
         pass
 
-    def create(self, arg):
-        
+    def check_for_class(self, args):
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        else:
+            try:
+                check = eval("{}()".format(args[0]))
+                dict = storage.all()
+                del dict[args[0] + "." + check.id]
+                return True
+            except:
+                print("** class doesn't exist **")
+                return False
 
+    def check_for_id(self, args):
+        if len(args) != 2:
+            print("** instance id missing **")
+            return False
+
+        key = args[0] + "." + args[1]
+        if key in storage.all():
+            return True
+        else:
+            print("** no instance found **")
+            return False
+
+    def do_create(self, args):
+        className = args.split()
+        if self.check_for_class(className):
+            new_instance = eval("{}()".format(className[0]))
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        class_id = arg.split()
+        if self.check_for_class(class_id) and self.check_for_id(class_id):
+            dict = storage.all()
+            print("{}".format(dict[class_id[0] + "." + class_id[1]]))
+
+    def do_destroy(self, args):
+        class_id = args.split()
+        if self.check_for_class(class_id) and self.check_for_id(class_id):
+            dict = storage.all()
+            del dict[class_id[0] + "." + class_id[1]]
+            storage.save()
+
+    def do_all(self, args):
+        newlist = []
+        args_sp = args.split()
+        for key, value in storage.all().items():
+            className = key.split(".")
+            if className[0] == args_sp[0]:
+                newlist.append(value.__str__())
+                """print("->{}".format(value))"""
+        print(newlist)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
